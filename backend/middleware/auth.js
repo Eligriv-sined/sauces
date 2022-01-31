c5175f9341f815e02fd847bd62564df3a3d48fa9
Eligriv-Sined <vd5988@gmail.com>
2022-01-31 09:19:25 +0100

    coms

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,17 +1,17 @@
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken"); // Impoter le module json web token
 
-module.exports= (req , res ,next ) => {
-    try{
-        const token = req.headers.authorization.split(' ')[1];
-        const decodedToken = jwt.verify(token, 'Random_token_secret');
-        const userId = decodedToken.userId;
-        req.auth ={userId};
-        if(req.body.userId && req.body.userId !== userId){
-            throw 'User ID non valable'
-        }else{
-            next();
-        }
-    } catch(error){
-        res.status(401).json({ error: error |'requete non authentifier' })
+module.exports = (req, res, next) => {// Fonction autorisation
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+    const decodedToken = jwt.verify(token, "Random_token_secret");
+    const userId = decodedToken.userId;
+    req.auth = { userId }; // Création d'un objet auth
+    if (req.body.userId && req.body.userId !== userId) {
+      throw "User ID non valable";
+    } else {
+      next();
     }
-}
+  } catch (error) {
+    res.status(401).json({ error: error | "requete non authentifier" });
+  }
+};
--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -1,20 +1,21 @@
-const multer = require('multer');
+const multer = require("multer");// Création de configuration pour multer - diskStorage => veut dire qu'on enregistre sur le disque
 
-const MIME_TYPES = {
-  'image/jpg': 'jpg',
-  'image/jpeg': 'jpg',
-  'image/png': 'png'
+const MIME_TYPES = {// Dictionnaire MIME_TYPES qui sera un objet
+  "image/jpg": "jpg",
+  "image/jpeg": "jpg",
+  "image/png": "png",
 };
 
-const storage = multer.diskStorage({
+const storage = multer.diskStorage({// Création de configuration pour multer - diskStorage => veut dire qu'on enregistre sur le disque
   destination: (req, file, callback) => {
-    callback(null, 'images');
+    callback(null, "images");   // null pour dire qu'il n'ya pas eu d'erreur et images pour la destination
   },
-  filename: (req, file, callback) => {
-    const name = file.originalname.split(' ').join('_');
+  filename: (req, file, callback) => {// filename expliquera à multer quel nom de fichier il faut utiliser
+        // Génerer le nom, s'il ya espace dans le nom de fichier ça peut créer des problèmes au niveau serveur donc on split
+    const name = file.originalname.split(" ").join("_");
     const extension = MIME_TYPES[file.mimetype];
-    callback(null, name + Date.now() + '.' + extension);
-  }
+    callback(null, name + Date.now() + "." + extension);
+  },
 });
 
-module.exports = multer({storage: storage}).single('image');
+module.exports = multer({ storage: storage }).single("image");
