4a3a877b7ee1b43363a3f9a39032942f297d205a
Eligriv-Sined <vd5988@gmail.com>
2022-02-02 19:11:42 +0100

    modif secu

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const stuffRoutes = require('./routes/sauces');
-const userRoutes = require ('./routes/user');
-const path = require('path');
-
+const stuffRoutes = require("./routes/sauces");
+const userRoutes = require("./routes/user");
+const path = require("path");
+require("dotenv").config()
 
 mongoose
   .connect(
-    "mongodb+srv://virgile:852456@cluster0.urhrf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    process.env.moongo+"?retryWrites=true&w=majority",
     { useNewUrlParser: true, useUnifiedTopology: true }
   )
   .then(() => console.log("Connexion à MongoDB réussie !"))
@@ -16,17 +16,17 @@ mongoose
 
 const app = express();
 
-app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use("/images", express.static(path.join(__dirname, "images"))); //multer
 
 app.use(express.json());
 
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
+  res.setHeader("Access-Control-Allow-Origin", "*"); //accéder à notre API depuis n'importe quelle origine 
+  res.setHeader( //ajouter les headers mentionnés aux requêtes envoyées vers notre API 
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
   );
-  res.setHeader(
+  res.setHeader(//envoyer des requêtes avec les méthodes mentionnées
     "Access-Control-Allow-Methods",
     "GET, POST, PUT, DELETE, PATCH, OPTIONS"
   );
@@ -34,7 +34,7 @@ app.use((req, res, next) => {
 });
 
 app.use(bodyParser.json());
-app.use('/api/sauces' , stuffRoutes);
-app.use ('/api/auth' , userRoutes);
+app.use("/api/sauces", stuffRoutes);
+app.use("/api/auth", userRoutes);
 
 module.exports = app;
